7215aa26467b2d3a41e2b3c84ef332f746624351
r-jeffery-wall <rowjw97@gmail.com>
2024-04-03 21:56:18 +0100

    Sized heading.

--- src/features/header/Header.js
+++ src/features/header/Header.js
@@ -4,12 +4,13 @@ export const Header = () => {
   return (
     <Navbar className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand>
+        <Navbar.Brand className="fs-1">
           <img
             src="./img/icons8-reddit-50.png"
             width="50"
             height="50"
             alt="a plain outline of the reddit logo."
+            className="me-3"
           />
           PlainReddit
         </Navbar.Brand>
