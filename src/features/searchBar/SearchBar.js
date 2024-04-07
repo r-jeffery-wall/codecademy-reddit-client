7a51f414c0baa5591ca020f9cf9e3e8c0b46465e
r-jeffery-wall <rowjw97@gmail.com>
2024-04-07 16:29:44 +0100

    Made SearchBar have an uncontrolled input

--- src/features/searchBar/SearchBar.js
+++ src/features/searchBar/SearchBar.js
@@ -1,9 +1,12 @@
 import { InputGroup, Form, Button } from "react-bootstrap"
+import { useState } from 'react';
 
 export const SearchBar = () => {
+    const [search, setSearch] = useState('');
+
     return (
         <InputGroup className="" style={{width: "375px"}}>
-            <Form.Control placeholder="search" aria-label="search" aria-describedby="search" />
+            <Form.Control placeholder="search" aria-label="search" aria-describedby="search" value={search} onChange={(e) => setSearch(e.target.value)}/>
             <Button variant="primary"><i className="fas fa-search" /></Button>
         </InputGroup>
     )
