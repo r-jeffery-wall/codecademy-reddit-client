b9d8258d7c2303c960e5b555a4aba49f33cb7ff3
r-jeffery-wall <rowjw97@gmail.com>
2024-04-13 15:04:11 +0100

    Removed auth slice

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Header } from './features/header/Header';
 import './App.scss';
 import { PostsList } from './features/posts/PostsList';
-import { useDispatch, useSelector } from 'react-redux';
-import { authenticate, selectAccessToken } from './features/auth/AuthSlice';
 
 function App() {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(authenticate())
-  })
-
   return (
     <div className="App">
       <Header />
