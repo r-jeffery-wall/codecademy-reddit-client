aa5abaa5f30875e71f37132991ebfa240bc06f1d
r-jeffery-wall <rowjw97@gmail.com>
2024-04-13 14:40:54 +0100

    Added auth slice.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Header } from './features/header/Header';
 import './App.scss';
 import { PostsList } from './features/posts/PostsList';
+import { useDispatch, useSelector } from 'react-redux';
+import { authenticate, selectAccessToken } from './features/auth/AuthSlice';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(authenticate())
+  })
+
   return (
     <div className="App">
       <Header />
--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
+import AuthSlice from '../features/auth/AuthSlice';
 import CommentsSlice from '../features/comments/CommentsSlice';
 import PopularSubsReducer from '../features/PopularSubs/PopularSubsSlice'
 import PostsSlice from '../features/posts/PostsSlice';
 
 export const store = configureStore({
   reducer: {
+    auth: AuthSlice,
     PopularSubs: PopularSubsReducer,
     posts: PostsSlice,
     comments: CommentsSlice
