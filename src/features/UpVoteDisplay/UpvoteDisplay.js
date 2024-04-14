5eeab53da98bccc5e1291febd5d61af9e3c4d066
r-jeffery-wall <rowjw97@gmail.com>
2024-04-14 16:55:49 +0100

    Refactored upvote display into its own component

---
+++ src/features/UpVoteDisplay/UpvoteDisplay.js
@@ -0,0 +1,12 @@
+export const UpvoteDisplay = ({ upvotes }) => {
+  return (
+    <div
+      className="mx-1 d-flex flex-column justify-content-center align-items-center"
+      style={{ width: "15%", fontSize: "12px" }}
+    >
+      <i className="fa-solid fa-angle-up" style={{color: "#ff4500"}} />
+      <p className="fw-bold m-0">{upvotes}</p>
+      <i className="fa-solid fa-angle-down" style={{color: "#7193ff"}}/>
+    </div>
+  );
+};
--- src/features/comments/CommentsList.js
+++ src/features/comments/CommentsList.js
@@ -1,16 +1,13 @@
 import { Accordion, AccordionBody } from "react-bootstrap";
 import { Comment } from './Comment';
+import { UpvoteDisplay } from "../UpVoteDisplay/UpvoteDisplay";
 
 export const CommentsList = ( {comments, upvotes} ) => {
 	return (
 		<Accordion className="p-2">
 			<Accordion.Item eventKey="0">
 				<Accordion.Header>
-					<div className="mx-1 d-flex flex-column justify-content-center align-items-center" style={{ width: "15%", fontSize: "12px" }}>
-						<i className="fa-solid fa-angle-up" />
-						<p className="fw-bold m-0">{upvotes}</p>
-						<i className="fa-solid fa-angle-down" />
-					</div>
+					<UpvoteDisplay upvotes={upvotes}/>
 					{comments.length} Comments
 				</Accordion.Header>
 				<AccordionBody className="p-0">
