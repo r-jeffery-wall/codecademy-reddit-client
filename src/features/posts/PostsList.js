ad246abf4f6f42dd2c5ff58dd8359eb297404dbb
r-jeffery-wall <rowjw97@gmail.com>
2024-04-14 12:53:14 +0100

    Implemented basic API integration for postsSlice.

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -1,13 +1,19 @@
-import { selectPosts } from "./PostsSlice"
+import { getPostsforSub, selectPosts } from "./PostsSlice"
 import { Post } from './Post';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from "react";
 
 export const PostsList = () => {
-    const posts = useSelector(selectPosts);
+    const dispatch = useDispatch();
+    const posts = useSelector(selectPosts); // This will eventually using routing to get the sub.
     
+    useEffect(() => {
+        dispatch(getPostsforSub(''));
+    }, [])
+
     return (
         <ul className="col-sm-6 m-auto p-3">
-            {posts.map(post => <Post title={post.title} imgSrc={post.imageSrc} text={post.text} upvotes={post.upvotes} key={post.title}/>)}
+            {posts.length > 0 ? posts.map(post => <Post title={post.title} imgSrc={post.imageSrc} text={post.text} upvotes={post.upvotes} key={post.title}/>) : <></>}
         </ul>
     )
 }
--- src/features/posts/PostsSlice.js
+++ src/features/posts/PostsSlice.js
@@ -1,24 +1,46 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+export const getPostsforSub = createAsyncThunk('posts/getPostsforSub', async (sub) => { // Accepts a reddit sub and provides a list of posts for that sub.
+    const response = await fetch(`https://api.reddit.com/${sub}`);
+    const json = await response.json();
+    const data = json.data.children.map(post => { // TODO: incorporate images to this.
+        return {
+            subreddit: post.data.subreddit_name_prefixed,
+            author: post.data.author_fullname,
+            title: post.data.title,
+            upvotes: post.data.ups,
+            key: post.data.name
+        }
+    })
+    return data;
+})
+
 
 const PostsSlice = createSlice({
     name: 'posts',
-    initialState: [{
-        title: 'Test Post',
-        imageSrc: './img/5616.avif',
-        text: 'Test post text.',
-        upvotes: 456,
-        comments: [] //TODO
-    },
-    {
-        title: 'Test Post',
-        text: 'Test post text.',
-        upvotes: 456,
-        comments: [] //TODO
-    }], // Dummy Values.
+    initialState: {
+        postsList: [],
+        postsLoading: false,
+        postsFailed: false
+    }, 
     reducers: {
-
+    },
+    extraReducers: {
+        [getPostsforSub.pending]: (state) => {
+            state.postsLoading = true;
+            state.postsFailed = false;
+        },
+        [getPostsforSub.fulfilled]: (state, action) => {
+            state.postsLoading = false;
+            state.postsFailed = false;
+            state.postsList = action.payload;
+        },
+        [getPostsforSub.rejected]: (state) => {
+            state.postsLoading = false;
+            state.postsFailed = true;
+        }
     }
 })
 
-export const selectPosts = (state) => state.posts; // TODO - expand this to get posts by Sub when API connection is implemented.
+export const selectPosts = (state) => state.posts.postsList; 
 export default PostsSlice.reducer;
