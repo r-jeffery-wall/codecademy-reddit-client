1776bda42f6e6addb779d09c64ae96e583067f9a
r-jeffery-wall <rowjw97@gmail.com>
2024-04-14 16:35:56 +0100

    Fleshed out data pulled from API for posts.

--- src/features/posts/PostsSlice.js
+++ src/features/posts/PostsSlice.js
@@ -1,46 +1,50 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
-export const getPostsforSub = createAsyncThunk('posts/getPostsforSub', async (sub) => { // Accepts a reddit sub and provides a list of posts for that sub.
+export const getPostsforSub = createAsyncThunk(
+  "posts/getPostsforSub",
+  async (sub) => {
+    // Accepts a reddit sub and provides a list of posts for that sub.
     const response = await fetch(`https://api.reddit.com/${sub}`);
     const json = await response.json();
-    const data = json.data.children.map(post => { // TODO: incorporate images to this.
-        return {
-            subreddit: post.data.subreddit_name_prefixed,
-            author: post.data.author_fullname,
-            title: post.data.title,
-            upvotes: post.data.ups,
-            key: post.data.name
-        }
-    })
+    const data = json.data.children.map((post) => {
+      return {
+        subreddit: post.data.subreddit_name_prefixed,
+        author: post.data.author_fullname,
+        title: post.data.title,
+        upvotes: post.data.ups,
+        key: post.data.name,
+        text: post.data.selftext,
+        url: post.data.url,
+      };
+    });
     return data;
-})
-
+  }
+);
 
 const PostsSlice = createSlice({
-    name: 'posts',
-    initialState: {
-        postsList: [],
-        postsLoading: false,
-        postsFailed: false
-    }, 
-    reducers: {
+  name: "posts",
+  initialState: {
+    postsList: [],
+    postsLoading: false,
+    postsFailed: false,
+  },
+  reducers: {},
+  extraReducers: {
+    [getPostsforSub.pending]: (state) => {
+      state.postsLoading = true;
+      state.postsFailed = false;
     },
-    extraReducers: {
-        [getPostsforSub.pending]: (state) => {
-            state.postsLoading = true;
-            state.postsFailed = false;
-        },
-        [getPostsforSub.fulfilled]: (state, action) => {
-            state.postsLoading = false;
-            state.postsFailed = false;
-            state.postsList = action.payload;
-        },
-        [getPostsforSub.rejected]: (state) => {
-            state.postsLoading = false;
-            state.postsFailed = true;
-        }
-    }
-})
+    [getPostsforSub.fulfilled]: (state, action) => {
+      state.postsLoading = false;
+      state.postsFailed = false;
+      state.postsList = action.payload;
+    },
+    [getPostsforSub.rejected]: (state) => {
+      state.postsLoading = false;
+      state.postsFailed = true;
+    },
+  },
+});
 
-export const selectPosts = (state) => state.posts.postsList; 
+export const selectPosts = (state) => state.posts.postsList;
 export default PostsSlice.reducer;
