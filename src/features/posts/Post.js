9b15f1364c82a83270220b66960344b3f3fe29c2
r-jeffery-wall <rowjw97@gmail.com>
2024-04-14 17:07:31 +0100

    Refactored Post display.

--- src/features/posts/Post.js
+++ src/features/posts/Post.js
@@ -3,12 +3,12 @@ import { CommentsList } from "../comments/CommentsList";
 import { selectComments } from "../comments/CommentsSlice";
 import { PostHeader } from "./PostHeader";
 
-export const Post = ({ title, imgSrc, text, upvotes, subreddit }) => {
+export const Post = ({ title, imgSrc, text, upvotes, subreddit, url }) => {
   const comments = useSelector(selectComments); // This will need to be modified to use the Post id to find the relevant comments.
 
   return (
     <div className="bg-body-tertiary border rounded my-3">
-      <PostHeader title={title} subreddit={subreddit} />
+      <PostHeader title={title} subreddit={subreddit} url={url} />
       {imgSrc ? (
         <img
           src={imgSrc}
--- src/features/posts/PostHeader.js
+++ src/features/posts/PostHeader.js
@@ -1,9 +1,9 @@
 import { Container } from 'react-bootstrap';
 
-export const PostHeader = ({title, subreddit}) => {
+export const PostHeader = ({title, subreddit, url}) => {
   return (
     <Container fluid className="fw-bold">
-      <h3 className="p-1">{title}</h3>
+      <a href={url} target='_blank' className='link-secondary'><h3 className='p-1'>{title}</h3></a>
       <p className="p-1">{subreddit}</p>
     </Container>
   );
