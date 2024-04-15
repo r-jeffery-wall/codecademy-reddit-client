c98382b178aa1702390d983fea68104b214eb477
r-jeffery-wall <rowjw97@gmail.com>
2024-04-15 11:20:05 +0100

    Created PostMedia component.

--- src/features/posts/Post.js
+++ src/features/posts/Post.js
@@ -2,22 +2,15 @@ import { useSelector } from "react-redux";
 import { CommentsList } from "../comments/CommentsList";
 import { selectComments } from "../comments/CommentsSlice";
 import { PostHeader } from "./PostHeader";
+import { PostMedia } from "./PostMedia";
 
-export const Post = ({ title, imgSrc, text, upvotes, subreddit, url }) => {
+export const Post = ({ title, text, upvotes, subreddit, url }) => {
   const comments = useSelector(selectComments); // This will need to be modified to use the Post id to find the relevant comments.
 
   return (
     <div className="bg-body-tertiary border rounded my-3">
       <PostHeader title={title} subreddit={subreddit} url={url} />
-      {imgSrc ? (
-        <img
-          src={imgSrc}
-          className="rounded m-auto"
-          style={{ maxWidth: "95%", height: "auto", display: "block" }}
-        />
-      ) : (
-        <></>
-      )}
+      <PostMedia media={url} />
       <p className="p-1">{text}</p>
       <CommentsList comments={comments} upvotes={upvotes} />
     </div>
---
+++ src/features/posts/PostMedia.js
@@ -0,0 +1,33 @@
+import {Container} from 'react-bootstrap';
+
+export const PostMedia = ({ media }) => {
+  if (media.includes("i.redd.it")) {
+    return (
+      <img
+        src={media}
+        className="rounded m-auto"
+        style={{ maxWidth: "95%", height: "auto", display: "block" }}
+      />
+    );
+  } else if (media.includes("youtu.be")) {
+    return (
+      <Container className='ratio ratio-16x9'>
+        <iframe
+          width="560"
+          height="315"
+          src={media}
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerpolicy="strict-origin-when-cross-origin"
+          allowfullscreen
+          className="rounded m-auto p-1"
+        ></iframe>
+      </Container>
+    );
+  } else if (media.includes("v.redd.it")) {
+    return <p className='p-1 text-center'>Reddit video is currently unsupported. Please click the post link to view the video on Reddit.</p>
+  } else {
+    return <></>
+  }
+};
