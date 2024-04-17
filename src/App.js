8b62a9cf0a76f5b922683e591a46899ff520abfa
r-jeffery-wall <rowjw97@gmail.com>
2024-04-17 14:38:13 +0100

    Added footer byline.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Header } from "./features/header/Header";
+import { Container } from 'react-bootstrap';
 import "./App.scss";
 import { PostsList } from "./features/posts/PostsList";
 
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <Header />
       <PostsList />
+      <Container fluid className="bg-body-tertiary sticky-bottom border-secondary-subtle border text-center">Developed by <a href="https://rowanjefferywall.com" target="_blank">Rowan Jeffery-Wall</a> as part of a <a href="https://www.codecademy.com/" target="_blank">Codecademy</a> portfolio project.</Container>
     </div>
   );
 }
