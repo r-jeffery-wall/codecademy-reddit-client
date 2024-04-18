ea3b5acbf6e3f981f2be5231543d9c4dd0d391ae
r-jeffery-wall <rowjw97@gmail.com>
2024-04-18 12:37:17 +0100

    Wrote thunks and reducers for searching posts/subs.

--- src/features/PopularSubs/PopularSubsSlice.js
+++ src/features/PopularSubs/PopularSubsSlice.js
@@ -1,44 +1,76 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
-export const getPopularSubs = createAsyncThunk('PopularSubs/getPopularSubs', async () => {
-    const response = await fetch('https://api.reddit.com/subreddits/popular.json?limit=8');
-    const json = await response.json();
-    const data = json.data.children.map(sub => {
-        return {
-        name: sub.data.display_name,
-        url: `https://reddit.com/${sub.data.display_name_prefixed}`,
-        key: sub.data.display_name
-        }
-    });
-    return data;
-})
+const processResponse = async (response) => {
+  const json = await response.json();
+  const data = json.data.children.map((sub) => {
+    return {
+      name: sub.data.display_name,
+      url: `https://reddit.com/${sub.data.display_name_prefixed}`,
+      key: sub.data.display_name,
+    };
+  });
+  return data;
+};
+
+export const getPopularSubs = createAsyncThunk(
+  "PopularSubs/getPopularSubs",
+  async () => {
+    const response = await fetch(
+      "https://api.reddit.com/subreddits/popular.json?limit=8"
+    );
+    return processResponse(response);
+  }
+);
+
+export const searchSubs = createAsyncThunk(
+  "PopularSubs/searchSubs",
+  async (searchTerm) => {
+    const response = await fetch(
+      `https://api/reddit.com/subreddits/search?q=${searchTerm}&limit=10`
+    );
+    return processResponse(response);
+  }
+);
 
 const PopularSubsSlice = createSlice({
-    name: 'PopularSubs',
-    initialState: {
-        popularSubsList: [],
-        popularSubsLoading: false,
-        popularSubsFailed: false
+  name: "PopularSubs",
+  initialState: {
+    popularSubsList: [],
+    popularSubsLoading: false,
+    popularSubsFailed: false,
+  },
+  reducers: {},
+  extraReducers: {
+    [getPopularSubs.pending]: (state) => {
+      state.popularSubsLoading = true;
+      state.popularSubsFailed = false;
     },
-    reducers: {
+    [getPopularSubs.fulfilled]: (state, action) => {
+      state.popularSubsLoading = false;
+      state.popularSubsFailed = false;
+      state.popularSubsList = action.payload;
     },
-    extraReducers: {
-        [getPopularSubs.pending]: (state) => {
-            state.popularSubsLoading = true;
-            state.popularSubsFailed = false;
-        },
-        [getPopularSubs.fulfilled]: (state, action) => {
-            state.popularSubsLoading = false;
-            state.popularSubsFailed = false;
-            state.popularSubsList = action.payload;
-        },
-        [getPopularSubs.rejected]: (state) => {
-            state.popularSubsLoading = false;
-            state.popularSubsFailed = true;
-        }
-    }
-})
+    [getPopularSubs.rejected]: (state) => {
+      state.popularSubsLoading = false;
+      state.popularSubsFailed = true;
+    },
+    [searchSubs.pending]: (state) => {
+      state.popularSubsLoading = true;
+      state.popularSubsFailed = false;
+    },
+    [searchSubs.fulfilled]: (state, action) => {
+      state.popularSubsLoading = false;
+      state.popularSubsFailed = false;
+      state.popularSubsList = action.payload;
+    },
+    [searchSubs.rejected]: (state) => {
+      state.popularSubsLoading = false;
+      state.popularSubsFailed = true;
+    },
+  },
+});
 
 export const selectPopularSubs = (state) => state.PopularSubs.popularSubsList;
-export const isPopularSubsLoading = (state) => state.PopularSubs.popularSubsLoading;
+export const isPopularSubsLoading = (state) =>
+  state.PopularSubs.popularSubsLoading;
 export default PopularSubsSlice.reducer;
--- src/features/posts/PostsSlice.js
+++ src/features/posts/PostsSlice.js
@@ -1,25 +1,39 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
+const processResponse = async (response) => {
+  const json = await response.json();
+  const data = json.data.children.map((post) => {
+    return {
+      subreddit: post.data.subreddit_name_prefixed,
+      author: post.data.author,
+      timestamp: post.data.created,
+      title: post.data.title,
+      upvotes: post.data.score,
+      key: post.data.id,
+      text: post.data.selftext,
+      url: post.data.url,
+      numComments: post.data.num_comments,
+    };
+  });
+  return data;
+};
+
 export const getPostsforSub = createAsyncThunk(
   "posts/getPostsforSub",
   async (sub) => {
     // Accepts a reddit sub and provides a list of posts for that sub.
     const response = await fetch(`https://api.reddit.com/${sub}`);
-    const json = await response.json();
-    const data = json.data.children.map((post) => {
-      return {
-        subreddit: post.data.subreddit_name_prefixed,
-        author: post.data.author,
-        timestamp: post.data.created,
-        title: post.data.title,
-        upvotes: post.data.score,
-        key: post.data.id,
-        text: post.data.selftext,
-        url: post.data.url,
-        numComments: post.data.num_comments,
-      };
-    });
-    return data;
+    return processResponse(response);
+  }
+);
+
+export const searchPosts = createAsyncThunk(
+  "posts/searchPosts",
+  async (searchTerm) => {
+    const response = await fetch(
+      `https://api.reddit.com/?q=${searchTerm}&limit=10`
+    );
+    return processResponse(response);
   }
 );
 
@@ -45,6 +59,19 @@ const PostsSlice = createSlice({
       state.postsLoading = false;
       state.postsFailed = true;
     },
+    [searchPosts.pending]: (state) => {
+      state.postsLoading = true;
+      state.postsFailed = false;
+    },
+    [searchPosts.fulfilled]: (state, action) => {
+      state.postsLoading = false;
+      state.postsFailed = false;
+      state.postsList = action.payload;
+    },
+    [searchPosts.rejected]: (state) => {
+      state.postsLoading = false;
+      state.postsFailed = true;
+    }
   },
 });
 
