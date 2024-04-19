eaeeafd66b9dea594069147ecb64c40b7f4d732e
r-jeffery-wall <rowjw97@gmail.com>
2024-04-19 21:12:25 +0100

    Updated tests.

--- src/App.test.js
+++ src/App.test.js
@@ -3,13 +3,15 @@ import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
+import { getPopularSubs } from './features/PopularSubs/PopularSubsSlice';
+import { getPostsforSub } from './features/posts/PostsSlice';
 
-test('renders learn react link', () => {
+test('renders header.', () => {
   const { getByText } = render(
     <Provider store={store}>
       <App />
     </Provider>
   );
 
-  expect(getByText(/learn/i)).toBeInTheDocument();
+  expect(getByText(/ReadOnlyReddit/i)).toBeInTheDocument();
 });
